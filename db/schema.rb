94bcb2f9a2aaab9d087c3e82c22e4d9ef7c444f9
Kenny Cuoq <kenny.cuoq@gmail.com>
2018-11-03 14:01:04 +0800

    Create validation for restaurant and review models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_03_041457) do
+ActiveRecord::Schema.define(version: 2018_11_03_042438) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "address"
@@ -18,6 +18,16 @@ ActiveRecord::Schema.define(version: 2018_11_03_041457) do
     t.string "phone_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.integer "rating"
+    t.integer "restaurant_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
   end
 
 end
